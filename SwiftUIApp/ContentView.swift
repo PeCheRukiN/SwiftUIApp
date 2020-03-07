cedfc5af0beadc3c32a99381d676b235aa4cfb28
Artem Pecherukin <a.pecherukin@geex-arts.com>
2020-03-07 16:52:53 +0300

    Initial Commit

---
+++ SwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by Artem Pecherukin on 07.03.2020.
+//  Copyright © 2020 pecherukin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
